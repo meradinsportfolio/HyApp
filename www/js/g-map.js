b3f8039000c2764a906446f0a6db11cafab620e4
Michael Schouten <manschouten@hotmail.com>
2016-05-06 20:34:46 +0200

    google map display

---
+++ www/js/g-map.js
@@ -0,0 +1,53 @@
+var mapInit = false,
+	encounter = false,
+	map = null,
+	myMarker = null,
+	myLocation = null,
+	wild_pokemon = [];
+	pokeMarker = [];
+
+$(document).on("pageshow", "#home", function() {
+	navigator.geolocation.getCurrentPosition(function(position) {
+		// onSucces
+		// if the map is not initialized
+		if (!mapInit) {
+			// map is not initialized
+				// create new map
+
+				map = new google.maps.Map(document.getElementById("gmap"), {
+					center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+					zoom: 15,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				});
+				// player marker
+				myMarker = new google.maps.Marker({
+					position: {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude
+					},
+					map: map,
+					icon: "./img/mapIcon27x30.png",
+					title: "my location"
+				});
+				myMarker.setMap(map);
+
+				// map is initialized, set mapInit true
+				mapInit = true;
+
+				getWildPokemon(position.coords);
+
+		} else {
+			// map is initialized
+
+		}
+	}, function(error) {
+		// onError
+		console.log("code: " + error.code + "\n" +
+					"message: " + error.message + "\n");
+	}, {
+		// options
+		maximumAge: 3000,
+		timeout: 5000,
+		enableHighAccuracy: true
+	});
+});
